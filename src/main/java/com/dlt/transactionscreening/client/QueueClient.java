1549722afabed092f5f5c9aaa11ef88adcf9d983
Sunil Kumar <sunkumar19@publicisgroupe.net>
2019-12-15 23:03:57 +0530

    My First commit

---
+++ src/main/java/com/dlt/transactionscreening/client/QueueClient.java
@@ -0,0 +1,19 @@
+package com.dlt.transactionscreening.client;
+// Include the following imports to use Service Bus APIs
+import com.google.gson.reflect.TypeToken;
+import com.microsoft.azure.servicebus.*;
+import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
+import com.google.gson.Gson;
+
+import static java.nio.charset.StandardCharsets.*;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.concurrent.*;
+
+import org.apache.commons.cli.*;
+public class QueueClient {
+    public static void main(String a[]){
+
+    }
+}
